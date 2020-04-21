ce7a27c3f9b8121db095ffcae965a0add869f27e
Po <reedgwhite@hotmail.com>
2020-04-21 00:54:07 -0500

    added some HTML effects to email

--- mail_demo.py
+++ mail_demo.py
@@ -1,27 +1,33 @@
 import os
 import smtplib
+from email.message import EmailMessage
+
 from secrets import TT_email_user, TT_email_password
 
 EMAIL_ADDRESS  = TT_email_user
 EMAIL_PASSWORD = TT_email_password
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
+msg = EmailMessage()
+msg['Subject'] = 'Tea Time'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'reedgwhite@hotmail.com'
 
-    # encrypt traffic
-    smtp.starttls()
+# plain text email
+msg.set_content('New tee time booked for you!')
 
-    # reidentify as an encrypted connection
-    smtp.ehlo()
+# html email
+msg.add_alternative("""\
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1 style="color:#56b000;"> It's Tea Time!</h1>
+            <p> Congrats, you've got a new tee time booked! </p>
+        </body>
+    </html>
+    """, subtype='html')
 
-    print(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     # login to mail server
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
-    subject = 'Tea Time'
-    body    = 'New tee time booked for you'
-
-    msg = f'Subject: {subject}\n\n{body}'
-
-    smtp.sendmail(EMAIL_ADDRESS, 'reedgwhite@hotmail.com', msg)
+    smtp.send_message(msg)
